443873de3dd1b637ad32434ce74a9880319572d1
Trishal <trishalmakhija@gmail.com>
2024-06-25 21:39:09 +0530

    fixes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const dbConnect = require("./config/dbconnect");
+const dbConnect = require("./config/dbConnect");
 const app = express();
 const dotenv = require("dotenv").config();
 const PORT = (process.env.PORT || 4000);
